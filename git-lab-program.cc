d5adea1ac0ebf75c3c54a46300c77b1d3bc26860
Aaron Bundy <ab748918@ohio.edu>
2020-09-02 18:15:41 -0400

    I think I did this right

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Aaron Bundy
+ *        Date: 9/2/2020
+ * Description: clone repository 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
